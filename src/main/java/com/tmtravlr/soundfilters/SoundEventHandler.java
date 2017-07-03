a6a6dd505f90b9f6d295602a6fefe9976899b485
HYPERBOB <tmtravlrsmail@gmail.com>
2017-07-02 19:31:04 -0700

    - Fixed crash when loading on the server - Fixed crash with the sourceOcclusionMap

--- src/main/java/com/tmtravlr/soundfilters/SoundEventHandler.java
+++ src/main/java/com/tmtravlr/soundfilters/SoundEventHandler.java
@@ -15,44 +15,42 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import paulscode.sound.Source;
 
 /**
- * This class handles the sound events, specifically when sounds start
- * playing. It adds the sounds to the source map. Created to solve the
- * "random movements" problem that happened occasionally.
+ * This class handles the sound events, specifically when sounds start playing.
+ * It adds the sounds to the source map. Created to solve the "random movements"
+ * problem that happened occasionally.
  * 
  * @author Rebeca Rey
  * @Date 2014
  */
-public class SoundEventHandler
-{
+public class SoundEventHandler {
 	@SubscribeEvent
-	 public void playSoundEventHandler(PlaySoundEvent event) 
-	{
-		if (event.getSound().getAttenuationType() != AttenuationType.NONE) 
-		{ 
+	public void playSoundEventHandler(PlaySoundEvent event) {
+		if (event.getSound().getAttenuationType() != AttenuationType.NONE) {
 			this.addSourceToMap(event.getSound().getXPosF(), event.getSound().getYPosF(), event.getSound().getZPosF());
 		}
 	}
-    
-    /**
-     * Finds the occlusion amount for the given location, and adds a new source to
-     * the sourceOccusionMap, with the source as null (will get set to the actual source
-     * the first time it runs through the SoundTickHandler).
-     * 
-     * @param x x position of the source
-     * @param y y position of the source
-     * @param z z position of the source
-     */
-    private void addSourceToMap(float x, float y, float z)
-    {
-        Minecraft mc = Minecraft.getMinecraft();
-        double amount = 0.0D;
 
-        if (mc.theWorld != null && mc.thePlayer != null)
-        {
-            amount = SoundTickHandler.getSoundOcclusion(mc.theWorld, new Vec3d((double)x, (double)y, (double)z), new Vec3d(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ));
-        }
+	/**
+	 * Finds the occlusion amount for the given location, and adds a new source
+	 * to the sourceOccusionMap, with the source as null (will get set to the
+	 * actual source the first time it runs through the SoundTickHandler).
+	 * 
+	 * @param x
+	 *            x position of the source
+	 * @param y
+	 *            y position of the source
+	 * @param z
+	 *            z position of the source
+	 */
+	private void addSourceToMap(float x, float y, float z) {
+		Minecraft mc = Minecraft.getMinecraft();
+		double amount = 0.0D;
 
-        SoundTickHandler.sourceOcclusionMap.put(new ComparablePosition(x, y, z), new DoubleWithTimeout((Source)null, amount, 10));
+		if (mc.theWorld != null && mc.thePlayer != null) {
+			amount = SoundTickHandler.getSoundOcclusion(mc.theWorld, new Vec3d((double) x, (double) y, (double) z), new Vec3d(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ));
+		}
 
-    }
+		SoundTickHandler.sourceOcclusionMap.put(new ComparablePosition(x, y, z), new DoubleWithTimeout((Source) null, amount, 10));
+
+	}
 }
